8f890e84f256d5d670f1a7aa82b31dd7ec29bd04
Kasparex <patreontv@gmail.com>
2025-12-05 20:02:22 +0100

    Fix build import path in Cloudflare Pages functions

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -1,5 +1,5 @@
 import { createRequestHandler } from "@remix-run/cloudflare-pages";
-import * as build from "../build";
+import * as build from "../build/index.js";
 
 export const onRequest = createRequestHandler({
   build,
